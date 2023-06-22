c700b0e539519ad2a0a8dcbc848ac81159874af9
David O'Connor <david.alan.oconnor@gmail.com>
2023-06-22 10:12:42 -0400

    Remove half lib; use native f16

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 
 #![no_std]
 
-use core::sync::atomic::AtomicBool;
+use core::{mem, sync::atomic::AtomicBool};
 
 use num_enum::TryFromPrimitive;
 
@@ -108,27 +108,87 @@ impl CanBitrate {
 }
 
 /// 16-bit floating point
-/// Alternative to `half` lib, without bringing in a dep
+/// Alternative to `half` lib, without bringing in a dep.
 ///
 #[allow(non_camel_case_types)]
+#[derive(Debug)]
+/// We use this instead of the `half` lib due to binary size issues when using the lib.
 pub struct f16 {
     bits: u16,
 }
 
 impl f16 {
-    pub fn from_f32(val: f32) -> Self {
-        //https://stackoverflow.com/questions/1659440/32-bit-to-16-bit-floating-point-conversion
+    pub const fn from_f32(value: f32) -> Self {
+        // half's implementation
+        // https://github.com/starkat99/half-rs/blob/main/src/binary16/arch.rs
+        // TODO: Replace mem::transmute with to_bits() once to_bits is const-stabilized
+        // Convert to raw bytes
+        let x: u32 = unsafe { mem::transmute(value) };
 
-        let b = val as u32 + 0x0000_1000; // round-to-nearest-even: add last bit after truncated mantissa
-        let e = (b & 0x7F80_0000) >> 23; // exponent
-        let m = b & 0x007F_FFFF; // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
-        Self {
-            bits: ((b & 0x8000_0000) >> 16
-                | ((e > 112) as u32) * ((((e - 112) << 10) & 0x7C00) | m >> 13)
-                | (((e < 113) as u32) & ((e > 101) as u32))
-                    * ((((0x007FF000 + m) >> (125 - e)) + 1) >> 1)
-                | ((e > 143) as u32) * 0x7FFF) as u16,
-        } // sign : normalized : denormalized : saturate
+        // Extract IEEE754 components
+        let sign = x & 0x8000_0000u32;
+        let exp = x & 0x7F80_0000u32;
+        let man = x & 0x007F_FFFFu32;
+
+        // Check for all exponent bits being set, which is Infinity or NaN
+        if exp == 0x7F80_0000u32 {
+            // Set mantissa MSB for NaN (and also keep shifted mantissa bits)
+            let nan_bit = if man == 0 { 0 } else { 0x0200u32 };
+            return Self {
+                bits: ((sign >> 16) | 0x7C00u32 | nan_bit | (man >> 13)) as u16,
+            };
+        }
+
+        // The number is normalized, start assembling half precision version
+        let half_sign = sign >> 16;
+        // Unbias the exponent, then bias for half precision
+        let unbiased_exp = ((exp >> 23) as i32) - 127;
+        let half_exp = unbiased_exp + 15;
+
+        // Check for exponent overflow, return +infinity
+        if half_exp >= 0x1F {
+            return Self {
+                bits: (half_sign | 0x7C00u32) as u16,
+            };
+        }
+
+        // Check for underflow
+        if half_exp <= 0 {
+            // Check mantissa for what we can do
+            if 14 - half_exp > 24 {
+                // No rounding possibility, so this is a full underflow, return signed zero
+                return return Self {
+                    bits: half_sign as u16,
+                };
+            }
+            // Don't forget about hidden leading mantissa bit when assembling mantissa
+            let man = man | 0x0080_0000u32;
+            let mut half_man = man >> (14 - half_exp);
+            // Check for rounding (see comment above functions)
+            let round_bit = 1 << (13 - half_exp);
+            if (man & round_bit) != 0 && (man & (3 * round_bit - 1)) != 0 {
+                half_man += 1;
+            }
+            // No exponent for subnormals
+            return return Self {
+                bits: (half_sign | half_man) as u16,
+            };
+        }
+
+        // Rebias the exponent
+        let half_exp = (half_exp as u32) << 10;
+        let half_man = man >> 13;
+        // Check for rounding (see comment above functions)
+        let round_bit = 0x0000_1000u32;
+
+        let bits = if (man & round_bit) != 0 && (man & (3 * round_bit - 1)) != 0 {
+            // Round it
+            ((half_sign | half_exp | half_man) + 1) as u16
+        } else {
+            (half_sign | half_exp | half_man) as u16
+        };
+
+        Self { bits }
     }
 
     pub fn to_le_bytes(&self) -> [u8; 2] {
@@ -137,5 +197,9 @@ impl f16 {
 
     pub fn to_be_bytes(&self) -> [u8; 2] {
         self.bits.to_be_bytes()
+    }
+
+    pub fn as_u16(&self) -> u16 {
+        self.bits
     }
 }
